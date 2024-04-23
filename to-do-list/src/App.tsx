3db035891981f66884488d7f9000fca6fc35d387
marins <3simunovic.marin@gmail.com>
2024-04-23 21:40:29 +0200

    todo

--- to-do-list/src/App.tsx
+++ to-do-list/src/App.tsx
@@ -1,9 +1,47 @@
-function App() {
+import Button from "./components/button";
+import Layout from "./components/layout";
+
+type listType = {
+  tasks: string[];
+};
+
+const App = () => {
+  const data: listType = { tasks: ["operi sudje", "usisaj"] };
+
   return (
     <>
-      <div>todo</div>
+      <Layout>
+        <div>
+          <h1>TO-DO List</h1> <Button>Add task</Button>
+          <div>
+            {" "}
+            <select name="todolist" id="todolist">
+              <option value="Monday">Monday</option>
+              <option value="Tuesday">Tuesday</option>
+              <option value="Wednesday">Wednesday</option>
+              <option value="Thursday">Thursday</option>
+              <option value="Friday">Friday</option>
+              <option value="Saturday">Saturday</option>
+              <option value="Sunday">Sunday</option>
+            </select>
+          </div>
+          <div className="list">
+            <ul className="list__card">
+              {data.tasks.map((todo, index) => {
+                return (
+                  <div className="list__item">
+                    <li key={index}>{todo}</li>
+                    <Button>delete</Button>
+                    <Button>edit</Button>
+                  </div>
+                );
+              })}
+            </ul>
+          </div>
+        </div>
+      </Layout>
     </>
   );
-}
+};
 
 export default App;
---
+++ to-do-list/src/components/button.tsx
@@ -0,0 +1,15 @@
+import { ReactNode } from "react";
+
+type Props = {
+  children: ReactNode;
+};
+
+const Button = ({ children }: Props) => {
+  return (
+    <>
+      <button>{children}</button>
+    </>
+  );
+};
+
+export default Button;
